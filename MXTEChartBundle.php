9b90dd82d2e817f6976ca86d07dbc53b2dec18af
Maximo Sojo <maxsojo13@gmail.com>
2018-11-09 16:42:48 -0400

    refactor(Bundle): Se configura base bundle así como registro de composer y licencia.

---
+++ MXTEChartBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Máximo Sojo - package.
+ * 
+ * (c) https://maxtoan.github.io/
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace MXT\EChartBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * MXTEChartBundle
+ *
+ * @author Máximo Sojo <maxsojo13@gmail.com>
+ */
+class MXTEChartBundle extends Bundle
+{
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+MXTEChartBundle
+========================
+
+MXTEChartBundle desarrollado para Symfony 2.+, 3.+, proporciona una serie de gráficas.
+
+## Instalación
+
+``` bash
+$ composer require mxt/echart-bundle
+```
+
+## Licencia
+
+Este paquete está bajo la licencia MIT. Ver la licencia completa. [LICENSE](LICENSE)
+
